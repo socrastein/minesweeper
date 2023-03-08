76561fc28ed151a6b4c1c393f40f35c33f5ed82c
socrastein <31764357+socrastein@users.noreply.github.com>
2023-03-08 14:00:26 -0700

    Expand function to have win condition

--- src/components/gameBoard/endGame.js
+++ src/components/gameBoard/endGame.js
@@ -1,3 +1,10 @@
-export default function (){
-  console.log("Clicked a mine!");
+import gameState from "./gameState";
+
+export default function (win = false) {
+  gameState.gameOver = true;
+  if (!win) {
+    console.log("Clicked a mine!");
+  } else {
+    console.log("You win!");
+  }
 }
