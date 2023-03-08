326ce22eac984643597271d172cfc0c57ee844d0
socrastein <31764357+socrastein@users.noreply.github.com>
2023-03-08 13:59:22 -0700

    Add check for mouse over gameBoard b4 setting flag

--- src/components/gameBoard/setFlag.js
+++ src/components/gameBoard/setFlag.js
@@ -1,6 +1,9 @@
 import gameState from "./gameState";
 
 export default function (){
+  if(!gameState.mouseOverGameboard){
+    return
+  }
   if(gameState.hoveredCell.innerHTML == "🚩"){
     gameState.hoveredCell.innerHTML = "";
   } else gameState.hoveredCell.innerHTML = "🚩";
