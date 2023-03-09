e827ff90b1459de5a916105b0a273845c21d5d4b
socrastein <31764357+socrastein@users.noreply.github.com>
2023-03-09 01:12:47 -0700

    Replace red cell color w bomb emoji

--- src/components/gameBoard/endGame.js
+++ src/components/gameBoard/endGame.js
@@ -2,9 +2,13 @@ import gameState from "./gameState";
 
 export default function (win = false) {
   gameState.gameOver = true;
+  let mines = document.getElementsByClassName("mine");
+  for (let i in mines) {
+    mines[i].innerHTML = "💣";
+  }
   if (!win) {
-    console.log("Clicked a mine!");
-  } else {
     console.log("You win!");
+  } else {
+    console.log("Clicked a mine!");
   }
 }
